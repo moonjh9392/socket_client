163939365d5cfbad17d2a7e833acde9f73a0fa7e
moon <moonjh9392@gmail.com>
2022-11-10 11:01:21 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import { useRef, useState } from 'react';
 
+const AppStyle = styled.div`
+  padding: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const ws = new WebSocket('ws://localhost:8001');
 function App() {
+  const [nickname, setNickname] = useState('');
+  const [message, setMessage] = useState('');
+  const divRef = useRef();
+  // 메세지 전송
+  function sendMessage() {
+    const fullMessage = `${nickname}: ${message}`;
+
+    ws.send(fullMessage);
+  }
+
+  // 메세지 수신
+  function receiveMessage(event) {
+    const chat = document.createElement('div');
+    const message = document.createTextNode(event.data);
+    chat.appendChild(message);
+    divRef.current.appendChild(chat);
+  }
+  ws.onmessage = receiveMessage;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppStyle>
+      <h1>Hudi Chat</h1>
+      <div>
+        <input placeholder="닉네임" onChange={(e) => setNickname(e.target.value)} />
+        <input placeholder="메세지" onChange={(e) => setMessage(e.target.value)} />
+        <button onClick={sendMessage}>전송</button>
+      </div>
+
+      <div ref={divRef}></div>
+    </AppStyle>
   );
 }
 
